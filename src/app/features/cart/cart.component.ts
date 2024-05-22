e2d7c4520f039bdfbfaa5ec697a24073ca306125
Geffrey99 <146091159+Geffrey99@users.noreply.github.com>
2024-05-23 00:48:14 +0200

    Carrito-de-compras

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -3,22 +3,47 @@ import { CartService } from '../../services/features/cart.service';
 import { Product } from '../../services/producto';
 import { CurrencyPipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
-
+import { FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CurrencyPipe, CommonModule],
+  imports: [CurrencyPipe, CommonModule, ReactiveFormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent implements OnInit {
   cart: Product[] = [];
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,
+              private formBuilder:FormBuilder) {}
+ 
+items = this.cartService.getItems();
+
+checkoutForm = this.formBuilder.group({
+  name:'',
+  address:''
+});
+
+
+
+
+
 
   ngOnInit(): void {
       this.cartService.cart$.subscribe((cart: Product[]) => {
           this.cart = cart;
       });
   }
+
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Te llegara a tu correo el tickettt' + this.checkoutForm.value)
+  
+  this.checkoutForm.reset();
+  
+  }
+
+
 }
--- src/app/services/features/cart.service.ts
+++ src/app/services/features/cart.service.ts
@@ -9,7 +9,7 @@ import { BehaviorSubject } from 'rxjs';
 export class CartService {
     private cart: Product[] = [];
     private cartSubject = new BehaviorSubject<Product[]>([]);
-
+    items:Product[] = [];
     cart$ = this.cartSubject.asObservable();
 
     addToCart(product: Product): void {
@@ -21,8 +21,27 @@ export class CartService {
         return this.cart;
     }
 
-    clearCart(): void {
-        this.cart = [];
-        this.cartSubject.next(this.cart);
-    }
+    // clearCart(): void {
+    //     this.cart = [];
+    //     this.cartSubject.next(this.cart);
+    // }
+
+    //recopila los artículos que los usuarios agregan al carrito y devuelve cada artículo con su cantidad asociada.
+getItems() {
+    return this.items;
+
+
+    //método devuelve una serie vacía de artículos, lo que vacía el carrito.
+
+  
+  }
+ 
+  
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  
+
 }
